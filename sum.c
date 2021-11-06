26f091c343c708c9226f0f678e47e6f742932193
015-Aman <asc23062000@gmail.com>
2021-11-06 17:42:25 +0530

    sum of two number program is added.

---
+++ sum.c
@@ -0,0 +1,17 @@
+/*Program to add two numbers.*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+	int a,b,s;
+	printf("enter first number : ");
+	scanf("%d", &a);
+	printf("enter second number : ");
+	scanf("%d", &b);
+	s=a+b;
+	printf("Sum is : %d", s);
+	return 0;
+} 	
